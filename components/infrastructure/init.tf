53a2184611cac6f2c017167eab477cfd9b2882fb
taherkhan30 <taher.khan@hmcts.net>
2022-11-10 14:07:10 +0000

    starting tf from again

--- components/infrastructure/init.tf
+++ components/infrastructure/init.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.0.11"
+  required_version = ">= 1.3.2"
 
   backend "azurerm" {
   }
@@ -17,35 +17,35 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
-provider "azurerm" {
-  alias = "additional_hub"
-  features {}
-  skip_provider_registration = true
-  subscription_id            = var.additional_hub_subscription_id
-}
-
 #provider "azurerm" {
-#  alias = "data"
+#  alias = "additional_hub"
 #  features {}
-#  subscription_id = var.data_subscription
+#  skip_provider_registration = true
+#  subscription_id            = var.additional_hub_subscription_id
 #}
-
-provider "azurerm" {
-  alias = "dns_zone"
-  features {}
-  subscription_id = var.dns_zone_subscription_id
-}
-
-provider "azurerm" {
-  alias = "hub"
-  features {}
-  subscription_id = var.hub_subscription_id
-}
-
-provider "azurerm" {
-  alias = "mgmt"
-  features {}
-  subscription_id = var.mgmt_subscription_id
-}
+#
+##provider "azurerm" {
+##  alias = "data"
+##  features {}
+##  subscription_id = var.data_subscription
+##}
+#
+#provider "azurerm" {
+#  alias = "dns_zone"
+#  features {}
+#  subscription_id = var.dns_zone_subscription_id
+#}
+#
+#provider "azurerm" {
+#  alias = "hub"
+#  features {}
+#  subscription_id = var.hub_subscription_id
+#}
+#
+#provider "azurerm" {
+#  alias = "mgmt"
+#  features {}
+#  subscription_id = var.mgmt_subscription_id
+#}
 
 
