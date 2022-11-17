9a639dbf94d308b03d18ee58c75a62971f7743c5
taherkhan30 <taher.khan@hmcts.net>
2022-11-17 13:18:42 +0000

    added resource group and networking configuration

--- components/infrastructure/02-resource-groups.tf
+++ components/infrastructure/02-resource-groups.tf
@@ -1,5 +1,5 @@
 #panorama-sbox-ukw-rg
 resource "azurerm_resource_group" "ctsc_rg" {
-  name     = local.rg_name
+  name     = "${local.rg_name}-rg"
   location = var.location
 }
--- components/infrastructure/04-networking.tf
+++ components/infrastructure/04-networking.tf
@@ -1,7 +1,4 @@
-
-
-#subnets
-
+# Subnet
 data "azurerm_key_vault" "hub_azure_keyvault" {
   name                = "hmcts-infra-hub-${var.environment}"
   resource_group_name = "hmcts-infra-hub-${var.environment}"
@@ -22,17 +19,14 @@ data "azurerm_virtual_network" "vnet_data" {
   resource_group_name = data.azurerm_key_vault_secret.hub_rg_name.value
 }
 
-
-resource "azurerm_subnet" "subnet-pbi-data-gateway" {
+resource "azurerm_subnet" "pbi-data-gateway" {
   name                 = "pbi-data-gateway"
   resource_group_name  = data.azurerm_virtual_network.vnet_data.resource_group_name
   virtual_network_name = data.azurerm_virtual_network.vnet_data.name
   address_prefixes     = [var.subnet_prefix]
-
 }
 
-#nsg
-
+# Network Security Group
 resource "azurerm_network_security_group" "nsg_ctsc" {
   name                = "nsg_ctsc"
   location            = azurerm_resource_group.ctsc_rg.location
@@ -40,10 +34,8 @@ resource "azurerm_network_security_group" "nsg_ctsc" {
   tags                = local.common_tags
 }
 
-
-#nsg association
-
-resource "azurerm_subnet_network_security_group_association" "nsg_mgmt" {
-  subnet_id                 = azurerm_subnet.subnet-pbi-data-gateway.id
+# Network Security Group Association
+resource "azurerm_subnet_network_security_group_association" "nsg_ctsc_association" {
+  subnet_id                 = azurerm_subnet.pbi-data-gateway.id
   network_security_group_id = azurerm_network_security_group.nsg_ctsc.id
 }
