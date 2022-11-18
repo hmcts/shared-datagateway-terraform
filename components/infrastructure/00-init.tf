298b5db398d6b40450d1d056d9884412b6b44f0a
Felix Eyetan <peyetan@yahoo.com>
2022-11-18 23:04:11 +0000

    removing comments

--- components/infrastructure/00-init.tf
+++ components/infrastructure/00-init.tf
@@ -25,35 +25,3 @@ provider "azurerm" {
 }
 
 
-#provider "azurerm" {
-#  alias = "additional_hub"
-#  features {}
-#  skip_provider_registration = true
-#  subscription_id            = var.additional_hub_subscription_id
-#}
-#
-##provider "azurerm" {
-##  alias = "data"
-##  features {}
-##  subscription_id = var.data_subscription
-##}
-#
-#provider "azurerm" {
-#  alias = "dns_zone"
-#  features {}
-#  subscription_id = var.dns_zone_subscription_id
-#}
-#
-#provider "azurerm" {
-#  alias = "hub"
-#  features {}
-#  subscription_id = var.hub_subscription_id
-#}
-#
-#provider "azurerm" {
-#  alias = "mgmt"
-#  features {}
-#  subscription_id = var.mgmt_subscription_id
-#}
-
-
