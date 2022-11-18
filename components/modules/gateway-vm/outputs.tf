2a137c40c5cc3c33f8999a8446f4cf95532a7de7
Felix Eyetan <peyetan@yahoo.com>
2022-11-18 23:10:52 +0000

    Adding module outputs

--- components/modules/gateway-vm/outputs.tf
+++ components/modules/gateway-vm/outputs.tf
@@ -1,1 +1,17 @@
-# testing123
+# Output - vm provisioned
+output "ctsc_vm_name" {
+  description = "Provisioned CTSC vm name"
+  value       = join("", azurerm_windows_virtual_machine.ctsc_vm.*.name)
+}
+
+# Output - vm to private ip
+output "ctsc_vm_private_ip_address" {
+  description = "Provisioned vm private ip"
+  value       = join("", azurerm_windows_virtual_machine.ctsc_vm.*.private_ip_address)
+}
+
+# Output - vm resource-id
+output "ctsc_vm_resource_id" {
+  description = "Provisioned vm resource-id"
+  value       = join("", azurerm_windows_virtual_machine.ctsc_vm.*.id)
+}
