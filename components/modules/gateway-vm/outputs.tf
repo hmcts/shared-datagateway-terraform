d8319f0e0638a1f80277a04ba7a42fa6aa418707
Felix Eyetan <peyetan@yahoo.com>
2022-11-21 13:05:16 +0000

    updating module output

--- components/modules/gateway-vm/outputs.tf
+++ components/modules/gateway-vm/outputs.tf
@@ -1,17 +1,17 @@
 # Output - vm provisioned
 output "ctsc_vm_name" {
   description = "Provisioned CTSC vm name"
-  value       = join("", azurerm_windows_virtual_machine.ctsc_vm.*.name)
+  value       =  toset([for vm in azurerm_windows_virtual_machine.ctsc_vm : vm.name])
 }
 
 # Output - vm to private ip
 output "ctsc_vm_private_ip_address" {
   description = "Provisioned vm private ip"
-  value       = join("", azurerm_windows_virtual_machine.ctsc_vm.*.private_ip_address)
+  value       = toset([for vm in azurerm_windows_virtual_machine.ctsc_vm : vm.private_ip_address])
 }
 
 # Output - vm resource-id
 output "ctsc_vm_resource_id" {
   description = "Provisioned vm resource-id"
-  value       = join("", azurerm_windows_virtual_machine.ctsc_vm.*.id)
+  value       = toset([for vm in azurerm_windows_virtual_machine.ctsc_vm : vm.id])
 }
