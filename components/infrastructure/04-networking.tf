828cfc3a2211b9f8da88a0e2c52fedfd598e00fb
Felix Eyetan <peyetan@yahoo.com>
2022-11-22 10:14:11 +0000

    Adding outbound nsg rules and minor updates to configuration

--- components/infrastructure/04-networking.tf
+++ components/infrastructure/04-networking.tf
@@ -19,8 +19,8 @@ data "azurerm_virtual_network" "vnet_data" {
   resource_group_name = data.azurerm_key_vault_secret.hub_rg_name.value
 }
 
-resource "azurerm_subnet" "pbi-data-gateway" {
-  name                 = "pbi-data-gateway"
+resource "azurerm_subnet" "gateway_subnet" {
+  name                 = "ctsc-data-gateway"
   resource_group_name  = data.azurerm_virtual_network.vnet_data.resource_group_name
   virtual_network_name = data.azurerm_virtual_network.vnet_data.name
   address_prefixes     = [var.subnet_prefix]
@@ -28,7 +28,7 @@ resource "azurerm_subnet" "pbi-data-gateway" {
 
 # Network Security Group
 resource "azurerm_network_security_group" "nsg_ctsc" {
-  name                = "nsg_ctsc"
+  name                = local.nsg_name
   location            = azurerm_resource_group.ctsc_rg.location
   resource_group_name = azurerm_resource_group.ctsc_rg.name
   tags                = local.common_tags
@@ -36,6 +36,25 @@ resource "azurerm_network_security_group" "nsg_ctsc" {
 
 # Network Security Group Association
 resource "azurerm_subnet_network_security_group_association" "nsg_ctsc_association" {
-  subnet_id                 = azurerm_subnet.pbi-data-gateway.id
+  subnet_id                 = azurerm_subnet.gateway_subnet.id
   network_security_group_id = azurerm_network_security_group.nsg_ctsc.id
+}
+
+# Network Security group rule
+resource "azurerm_network_security_rule" "ctsc_nsg_rules" {
+  for_each = {
+    for rule in local.nsg_security_rules : rule.name => rule
+  }
+  name                        = each.value.name
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = each.value.protocol
+  source_port_range           = each.value.source_port_range
+  destination_port_ranges     = each.value.destination_port_ranges
+  source_address_prefixes     = each.value.source_address_prefixes
+  destination_address_prefix  = each.value.destination_address_prefix
+  resource_group_name         = azurerm_resource_group.ctsc_rg.name
+  network_security_group_name = local.nsg_name
+  description                 = each.value.description
 }
--- components/infrastructure/05-virtual-machine.tf
+++ components/infrastructure/05-virtual-machine.tf
@@ -4,7 +4,7 @@ module "ctsc" {
   environment         = var.environment
   ctsc_rg_location    = azurerm_resource_group.ctsc_rg.location
   ctsc_rg_name        = azurerm_resource_group.ctsc_rg.name
-  subnet_id           = azurerm_subnet.pbi-data-gateway.id
+  subnet_id           = azurerm_subnet.gateway_subnet.id
   vm_zones            = var.vm_zones
   vm_admin_user       = data.azurerm_key_vault_secret.vm_admin_user.value
   vm_admin_password   = data.azurerm_key_vault_secret.vm_admin_password.value
