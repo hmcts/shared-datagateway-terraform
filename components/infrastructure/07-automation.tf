1ab86c9d0174c5cc4b78fecbc34383ed2b8cf34f
mananpatel26 <manan.patel@kainos.com>
2022-11-29 12:48:45 +0000

    Defining variables for automation account

--- components/infrastructure/07-automation.tf
+++ components/infrastructure/07-automation.tf
@@ -1,11 +1,8 @@
-data "azurerm_resource_group" "ctsc_rg" {
-}
 resource "azurerm_automation_account" "ctsc_auto" {
   name                = "ctsc-dgw-automation"
   location            = var.location
-  resource_group_name = ctsc_rg.resource_group_name
+  resource_group_name = azurerm_resource_group.ctsc_rg.name
   sku_name_workspace  = "PerGB2018"
   log_retention_days  = "30"
-
-  tags = merge(local.common_tags, local.extra_tags, local.enforced_tags)
+  tags                = local.common_tags
 }
