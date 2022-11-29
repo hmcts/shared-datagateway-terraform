183231eba317bcabc6649a3a8e122fdd41eeae11
mananpatel26 <manan.patel@kainos.com>
2022-11-29 15:59:18 +0000

    Defining variables for automation account

--- components/infrastructure/07-automation.tf
+++ components/infrastructure/07-automation.tf
@@ -5,3 +5,17 @@ resource "azurerm_automation_account" "ctsc_auto" {
   sku_name            = var.sku_name
   tags                = local.common_tags
 }
+resource "azurerm_log_analytics_workspace" "ctsc_workspace" {
+  name                = var.log_analytics_workspace_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.ctsc_rg.name
+  sku                 = var.sku_name_workspace
+  retention_in_days   = var.log_retention_days
+}
+
+# Associate workspace and automation account
+resource "azurerm_log_analytics_linked_service" "automation_workspace_assoc" {
+  resource_group_name = data.azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.ctsc_workspace.id
+  read_access_id      = azurerm_automation_account.ctsc_auto.id
+}
