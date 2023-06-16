e7439bfb92de18e80cf8f5549fce0e5788473797
mananpatel26 <manan.patel@kainos.com>
2023-06-16 14:21:17 +0100

    Adding support for prod environment

--- components/infrastructure/02-resource-groups.tf
+++ components/infrastructure/02-resource-groups.tf
@@ -1,5 +1,5 @@
 #panorama-sbox-ukw-rg
 resource "azurerm_resource_group" "shared-datagateway-rg" {
-  name     = var.environment == "prod" ? loca.rg_name_prod : local.rg_name
+  name     = var.environment == "prod" ? local.rg_name_prod : local.rg_name
   location = var.location
 }
