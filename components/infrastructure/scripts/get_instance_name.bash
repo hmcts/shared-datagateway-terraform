af1701d82831b51ae474c493d9486006e698045b
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-07-17 11:06:04 +0100

    updated commands

--- components/infrastructure/scripts/get_instance_name.bash
+++ components/infrastructure/scripts/get_instance_name.bash
@@ -3,8 +3,9 @@ set -ex
 # Get the parameters from Terraform
 RESOURCE_GROUP=$1
 VMSS_NAME=$2
+SUBSCRIPTION_ID=$3
 
-
+az account set --subscription=$SUBSCRIPTION_ID
 INSTANCE_NAME=$(az vmss list-instances -g $RESOURCE_GROUP -n $VMSS_NAME --query "sort_by([], &instanceId)[0].osProfile.computerName" -o tsv)
 
 
